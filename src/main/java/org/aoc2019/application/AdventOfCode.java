fcf17b2960f4783372c76f238e8bb827e3571afb
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2020-01-18 23:09:46 +0100

    refactor code

--- src/main/java/org/aoc2019/application/AdventOfCode.java
+++ src/main/java/org/aoc2019/application/AdventOfCode.java
@@ -3,27 +3,31 @@ package org.aoc2019.application;
 import org.aoc2019.day.*;
 import org.aoc2019.utils.File;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class AdventOfCode {
 
-    public static void main(String[] args) {
-        File file = new File("src/main/resources/day1.txt");
-        List<String> lines = file.readFile();
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.lines(Paths.get("src/main/resources/day1.txt"))
+                .collect(Collectors.toList());
         Day1 day1 = new Day1(lines);
         day1.returnTotalFuel();
 
-        file = new File("src/main/resources/day2.txt");
-        lines = file.readFile();
+        lines = Files.lines(Paths.get("src/main/resources/day2.txt"))
+                .collect(Collectors.toList());
         Day2 day2 = new Day2(lines);
         int result = day2.calculatePositionZero(12, 2);
         System.out.println("Result: " + result);
         result = day2.calculateNounAndVerb();
         System.out.println("Result: " + result);
 
-        file = new File("src/main/resources/day3.txt");
-        lines = file.readFile();
+        lines = Files.lines(Paths.get("src/main/resources/day3.txt"))
+                .collect(Collectors.toList());
         Day3 day3 = new Day3(lines);
 
         Optional<Integer> distance = day3.calculateDistanceBetweenWires();
@@ -42,8 +46,8 @@ public class AdventOfCode {
         passwords = day4.getPasswordPart2();
         System.out.println("Number of passwords - Part 2: " + passwords);
 
-        file = new File("src/main/resources/day5.txt");
-        lines = file.readFile();
+        lines = Files.lines(Paths.get("src/main/resources/day5.txt"))
+                .collect(Collectors.toList());
         Day5 day5 = new Day5(lines);
         result = day5.calculate();
         System.out.println("Result Day 5 Part 1: " + result);
---
+++ src/main/java/org/aoc2019/day/Day6.java
@@ -0,0 +1,21 @@
+package org.aoc2019.day;
+
+import org.aoc2019.utils.Intcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day5 extends Day {
+
+    public Day5(List<String> inputs) {
+        super(inputs);
+    }
+
+    public int calculate() {
+        int[] numbers = Arrays.stream(getInputs().get(0).split(","))
+                .mapToInt(Integer::parseInt).toArray();
+
+        Intcode intcode = new Intcode();
+        return intcode.calculate(numbers);
+    }
+}
